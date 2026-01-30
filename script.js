887ce04be8d38f66210caec2714e8321a0492f60
jihanaiysa <jihanaiys081@gmail.com>
2026-01-30 10:05:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function hubungiSaya() {
+  alert("Terima kasih sudah mengunjungi portofolio saya 😊");
+}
+
+function login() {
+  alert("Login berhasil!");
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+}
